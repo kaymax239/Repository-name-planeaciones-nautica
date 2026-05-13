6ed9e23f61eadbe104d09c783bf9d73d64c829cd
kaymax239 <TUEMAIL@gmail.com>
2026-05-13 08:55:20 -0600

    fix final map

--- app/Mapa.tsx
+++ app/Mapa.tsx
@@ -1,16 +1,9 @@
-"use client";// @ts-nocheck
-import { collection, onSnapshot } from "firebase/firestore";
-import { db } from "./firebase";
-import { useEffect, useState } from "react";
+// @ts-nocheck
+"use client";
+
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-});
+import "leaflet/dist/leaflet.css";
 
 const iconoBus = new L.Icon({
   iconUrl: "https://cdn-icons-png.flaticon.com/512/61/61231.png",
@@ -18,36 +11,24 @@ const iconoBus = new L.Icon({
 });
 
 export default function Mapa() {
-    const [buses, setBuses] = useState<any[]>([]);
-useEffect(() => {
-  const unsubscribe = onSnapshot(
-    collection(db, "autobuses"),
-    (snapshot) => {
-      const data = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
+  return (
+    <MapContainer
+      center={[22.2553, -97.8686]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: "500px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-      setBuses(data);
-    }
+      <Marker
+        position={[22.2553, -97.8686]}
+        icon={iconoBus}
+      >
+        <Popup>Ruta Centro</Popup>
+      </Marker>
+
+    </MapContainer>
   );
-
-  return () => unsubscribe();
-}, []);
- return (
-  <MapContainer
-    center={[22.2553, -97.8686]}
-    zoom={13}
-    scrollWheelZoom={true}
-    style={{ height: "500px", width: "100%" }}
-  >
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-
-    <Marker position={[22.2553, -97.8686]} icon={iconoBus}>
-      <Popup>Ruta Centro</Popup>
-    </Marker
-  <MapContainer
-);
 }
