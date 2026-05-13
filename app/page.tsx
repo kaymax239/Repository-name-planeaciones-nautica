8d41f0066bde1463b965cf0cddf1dbb09a07dad3
kaymax239 <TUEMAIL@gmail.com>
2026-05-13 10:36:24 -0600

    force update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="p-4">
       <h1 className="text-2xl font-bold mb-4">
-        Aplicación rutas autobuses
+        Aplicación rutas autobuses Tampico
       </h1>
 
       <Mapa />
