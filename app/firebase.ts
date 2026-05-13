949453c44e1bf15d8ba523bdaad133f0642c62fc
kaymax239 <TUEMAIL@gmail.com>
2026-05-13 12:59:19 -0600

    connect firebase

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,15 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyBK6MUMKR0tH5_DrVLP63s1LHZfLyImtFA",
   authDomain: "rutas-de-autobuses.firebaseapp.com",
   projectId: "rutas-de-autobuses",
-  storageBucket: "rutas-de-autobuses.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
+  storageBucket: "rutas-de-autobuses.firebasestorage.app",
+  messagingSenderId: "1045712715452",
+  appId: "1:1045712715452:web:00125ca248683ae4ab1ab9"
+};const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
